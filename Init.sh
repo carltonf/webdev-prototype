c431e9f79202dd3ad390b7486fc45d6970e6f597
Carl Xiong <xiongc05@gmail.com>
2015-10-16 11:32:33 +0800

    Add README and Init.sh for easy deployment.

---
+++ Init.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+prjdir="$1"
+# * Argument check
+if [[ -z "$prjdir" ]];then
+    echo -n 'Your project name (directory name): '
+    read prjdir
+fi
+
+if [[ -d "$prjdir" ]]; then
+    echo "Error: ${prjdir} already exists."
+    exit 1;
+fi
+
+# * Download
+TMPDIR=`mktemp --tmpdir=/tmp -d webdev-prototype-XXX` # force /tmp
+cd ${TMPDIR}
+wget -nv https://github.com/carltonf/webdev-prototype/archive/master.zip
+unzip master.zip 
+
+cd -
+mv ${TMPDIR}/webdev-prototype-master ${prjdir}
+
+# * Clean up
+rm -rf ${TMPDIR}
+
+# * Post init
+cd ${prjdir}
+git init
+# remove this init script itself.
+rm -f Init.sh
+
+npm install
